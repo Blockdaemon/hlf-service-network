5bf8c743c70cc885fcbd63e7ee68f2141b3de44c
Nye Liu <nye@blockdaemon.com>
2018-11-28 12:51:17 +0000

    Add cluster and user to context

--- k8s/setup-namespace.sh
+++ k8s/setup-namespace.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 kubectl create -f namespace.json
-kubectl config set-context dev --namespace=hlf-service-network
-#  --cluster=minikube \
-#  --user=$(USER)\
+kubectl config set-context dev --namespace=hlf-service-network --cluster=minikube --user=minikube
 kubectl config use-context dev
