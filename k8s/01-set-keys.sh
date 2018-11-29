7d7951d26941b29ea6e3dcbcdc15b0dc92fd9a30
Nye Liu <nye@blockdaemon.com>
2018-11-29 23:51:02 +0000

    Add orderer msp pubkeys, code cleanup

--- k8s/01-set-keys.sh
+++ k8s/01-set-keys.sh
@@ -7,7 +7,11 @@ ARTIFACTS=$HSN_HOME/artifacts
 CRYPTO_CONFIG=$HSN_HOME/crypto-config
 
 function set-secret () {
-	kubectl create secret generic $@ --save-config --dry-run -o json | kubectl apply -f -
+    kubectl create secret generic $@ --save-config --dry-run -o json | kubectl apply -f -
+}
+
+function set-configmap() {
+    kubectl create configmap $@ --save-config --dry-run -o json | kubectl apply -f -
 }
 
 # ca-server
@@ -18,20 +22,21 @@ set-secret ca --from-file=$CA_TLS_DIR/ca.org1.$DOMAIN-cert.pem --from-file=$CA_T
 ORDERER_DIR=$CRYPTO_CONFIG/ordererOrganizations/$DOMAIN/orderers/orderer0.$DOMAIN
 set-secret orderer-genesis-block --from-file=$ARTIFACTS/orderer0.genesis.block
 set-secret orderer-tls --from-file=$ORDERER_DIR/tls
+set-configmap orderer${peer}-msp \
+    --from-file="ca-cert.pem"="${ORDERER_DIR}/msp/cacerts/ca.${DOMAIN}-cert.pem" \
+    --from-file="tlsca-cert.pem"="${ORDERER_DIR}/msp/tlscacerts/tlsca.${DOMAIN}-cert.pem"  \
+    --from-file="admin-cert.pem"="${ORDERER_DIR}/msp/admincerts/Admin@${DOMAIN}-cert.pem" \
+    --from-file="peer-cert.pem"="${ORDERER_DIR}/msp/signcerts/orderer0.${DOMAIN}-cert.pem"
 set-secret orderer-msp-keystore --from-file=$ORDERER_DIR/msp/keystore
 
 # peers
 for peer in 0 1; do
     PEER_DIR=$CRYPTO_CONFIG/peerOrganizations/org1.$DOMAIN/peers/peer${peer}.org1.$DOMAIN
     set-secret peer${peer}-tls --from-file=$PEER_DIR/tls
+    set-configmap peer${peer}-msp \
+        --from-file="ca-cert.pem"="${PEER_DIR}/msp/cacerts/ca.org1.${DOMAIN}-cert.pem" \
+        --from-file="tlsca-cert.pem"="${PEER_DIR}/msp/tlscacerts/tlsca.org1.${DOMAIN}-cert.pem"  \
+        --from-file="admin-cert.pem"="${PEER_DIR}/msp/admincerts/Admin@org1.${DOMAIN}-cert.pem" \
+        --from-file="peer-cert.pem"="${PEER_DIR}/msp/signcerts/peer${peer}.org1.${DOMAIN}-cert.pem"
     set-secret peer${peer}-msp-keystore --from-file=$PEER_DIR/msp/keystore
-
-    MSP_PATH="${HSN_HOME}/crypto-config/peerOrganizations/org1.${DOMAIN}/peers/peer${peer}.org1.${DOMAIN}/msp"
-
-    exec kubectl create configmap peer${peer} \
-        --from-file="ca-cert.pem"="${MSP_PATH}/cacerts/ca.org1.hlf.blockdaemon.io-cert.pem" \
-        --from-file="admin-cert.pem"="${MSP_PATH}/admincerts/Admin@org1.hlf.blockdaemon.io-cert.pem" \
-        --from-file="peer-cert.pem"="${MSP_PATH}/signcerts/peer${peer}.org1.hlf.blockdaemon.io-cert.pem" \
-        --from-file="tlsca-cert.pem"="${MSP_PATH}/tlscacerts/tlsca.org1.hlf.blockdaemon.io-cert.pem" \
-        --dry-run -o json | kubectl apply -f -
 done
