503acba74b603a09780d7d23c16e8621fb59bc46
Nye Liu <nye@blockdaemon.com>
2018-12-01 03:39:31 +0000

    Add various labels to assist in selectors

--- k8s/services.sh
+++ k8s/services.sh
@@ -4,10 +4,10 @@ function expose() {
     kubectl expose deployment $@ --save-config --dry-run -o json | kubectl apply -f -
 }
 
-expose fabric-ca-deployment --port=7054 --name=ca
+expose fabric-ca-deployment --port=7054 --name=ca-org1
 expose fabric-orderer-deployment --port=7050 --name=orderer0
 
 for i in 0 1; do
-    expose fabric-peer${i}-deployment --port=7051,7053 --name=peer${i}-org1
-    #expose fabric-couch${i}-deployment --port=5984 --name=couch${i}-org1
+    #expose fabric-peer${i}-deployment --port=7051,7053 --name=peer${i}-org1 # no couchdb
+    expose fabric-peer${i}-deployment --port=7051,7053,5984 --name=peer${i}-org1 # with couchdb
 done
