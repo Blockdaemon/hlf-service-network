0ceb9009df1a9c405f1b014913014edb93f41c03
Nye Liu <nye@blockdaemon.com>
2018-12-01 16:02:55 +0000

    Put couchdb passwords in secrets

--- k8s/01-set-keys.sh
+++ k8s/01-set-keys.sh
@@ -14,6 +14,8 @@ function set-configmap() {
     kubectl create configmap $@ --save-config --dry-run -o json | kubectl apply -f -
 }
 
+PEM="${DOMAIN}-cert.pem"
+
 # ca-server
 CA_TLS_DIR=$CRYPTO_CONFIG/peerOrganizations/org1.$DOMAIN/ca
 set-secret ca --from-file=$CA_TLS_DIR/ca.org1.$DOMAIN-cert.pem --from-file=$CA_TLS_DIR/current_sk
@@ -25,10 +27,10 @@ set-secret orderer-genesis-block --from-file=$ARTIFACTS/orderer0.genesis.block
 
 set-secret orderer-tls --from-file=$ORDERER_DIR/tls
 set-configmap orderer${peer}-msp \
-    --from-file="ca-cert.pem"="${ORDERER_DIR}/msp/cacerts/ca.${DOMAIN}-cert.pem" \
-    --from-file="tlsca-cert.pem"="${ORDERER_DIR}/msp/tlscacerts/tlsca.${DOMAIN}-cert.pem"  \
-    --from-file="admin-cert.pem"="${ORDERER_DIR}/msp/admincerts/Admin@${DOMAIN}-cert.pem" \
-    --from-file="sign-cert.pem"="${ORDERER_DIR}/msp/signcerts/orderer0.${DOMAIN}-cert.pem"
+    --from-file="ca-cert.pem"="${ORDERER_DIR}/msp/cacerts/ca.${PEM}" \
+    --from-file="tlsca-cert.pem"="${ORDERER_DIR}/msp/tlscacerts/tlsca.${PEM}"  \
+    --from-file="admin-cert.pem"="${ORDERER_DIR}/msp/admincerts/Admin@${PEM}" \
+    --from-file="sign-cert.pem"="${ORDERER_DIR}/msp/signcerts/orderer0.${PEM}"
 set-secret orderer-msp-keystore --from-file=$ORDERER_DIR/msp/keystore
 
 # peers
@@ -36,9 +38,16 @@ for peer in 0 1; do
     PEER_DIR=$CRYPTO_CONFIG/peerOrganizations/org1.$DOMAIN/peers/peer${peer}.org1.$DOMAIN
     set-secret peer${peer}-tls --from-file=$PEER_DIR/tls
     set-configmap peer${peer}-msp \
-        --from-file="ca-cert.pem"="${PEER_DIR}/msp/cacerts/ca.org1.${DOMAIN}-cert.pem" \
-        --from-file="tlsca-cert.pem"="${PEER_DIR}/msp/tlscacerts/tlsca.org1.${DOMAIN}-cert.pem"  \
-        --from-file="admin-cert.pem"="${PEER_DIR}/msp/admincerts/Admin@org1.${DOMAIN}-cert.pem" \
-        --from-file="sign-cert.pem"="${PEER_DIR}/msp/signcerts/peer${peer}.org1.${DOMAIN}-cert.pem"
+        --from-file="ca-cert.pem"="${PEER_DIR}/msp/cacerts/ca.org1.${PEM}" \
+        --from-file="tlsca-cert.pem"="${PEER_DIR}/msp/tlscacerts/tlsca.org1.${PEM}"  \
+        --from-file="admin-cert.pem"="${PEER_DIR}/msp/admincerts/Admin@org1.${PEM}" \
+        --from-file="sign-cert.pem"="${PEER_DIR}/msp/signcerts/peer${peer}.org1.${PEM}"
     set-secret peer${peer}-msp-keystore --from-file=$PEER_DIR/msp/keystore
+
+    # couchdb
+    CDUSER=$(openssl rand -base64 32 | tr -cd '[:alpha:]')
+    CDPASS=$(openssl rand -base64 32 | tr -cd '[:alpha:]')
+    set-secret couchdb${peer}-creds \
+        --from-literal="user=${CDUSER}" \
+        --from-literal="password=${CDPASS}"
 done
