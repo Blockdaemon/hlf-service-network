11c7abe318c64afee75f00c68973720297aec9d3
Nye Liu <nye@blockdaemon.com>
2018-12-06 16:47:04 +0000

    More support for multi-org

--- config.mk
+++ config.mk
@@ -1,13 +1,17 @@
 HLF_VERSION=1.3.0
 # NOTE: use x86_64 for HLF_VERSION 1.0.5 and earlier, they have no amd64 tags
 HLF_ARCH=amd64
+
 ORGNAME=Blockdaemon
-SUBORG1NAME=Org1Blockdaemon
-SUBORG2NAME=Org2Blockdaemon
+DOMAIN=hlf.blockdaemon.io
+
+SUBORG1NAME=BlockdaemonOrg1
+SUBDOMAIN1=org1
+
+SUBORG2NAME=BlockdaemonOrg2
+SUBDOMAIN2=org2
+
 PROFILE=SingleMSP
 CONSORTIUM=FabricConsortium
-DOMAIN=hlf.blockdaemon.io
-SUBDOMAIN1=org1
-SUBDOMAIN2=org2
 NETWORKID=blockdaemon
 CHANNEL=blockdaemon
