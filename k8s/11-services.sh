c1ee7f2017fb95993b99631ed30032018915b7e3
Nye Liu <nye@blockdaemon.com>
2018-12-06 16:10:34 -0800

    Cosmetic renaming/reordring of deployments/pods/containers
    Make kubectl command lines a bit less verbose!

--- k8s/11-services.sh
+++ k8s/11-services.sh
@@ -12,12 +12,12 @@ function expose() {
     kubectl expose deployment "$1" "--port=$2" "--name=$3" $TYPE --save-config --dry-run -o json | kubectl apply -f -
 }
 
-expose fabric-ca-deployment 7054 ca-${SUBDOMAIN1}
-expose fabric-orderer-deployment 7050 orderer0
+expose fabric-ca 7054 ca-${SUBDOMAIN1}
+expose fabric-orderer 7050 orderer0
 
 for i in 0 1; do
-    expose fabric-peer${i}-deployment 7051,7053 peer${i}-${SUBDOMAIN1} # no couchdb
-    # expose fabric-peer${i}-deployment 7051,7053,5984 peer${i}-${SUBDOMAIN1} # with couchdb
+    expose fabric-peer${i} 7051,7053 peer${i}-${SUBDOMAIN1} # no couchdb
+    # expose fabric-peer${i} 7051,7053,5984 peer${i}-${SUBDOMAIN1} # with couchdb
 done
 
 echo "Starting forwarders (use ./forwarding.sh stop to stop them)"
