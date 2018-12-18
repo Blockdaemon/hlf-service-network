0ae6c642ac8d12fb722e737160d295690a385395
Nye Liu <nye@blockdaemon.com>
2018-12-18 15:54:36 -0800

    Read env properly

--- helm/start.sh
+++ helm/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+. ../k8s/functions
+
+read-env
+
 set -xe
 
-helm install orderer --set DomainName=hlf.blockdaemon.io
-helm install peer --set PeerNum=0 --set OrgName=prod --set DomainName=hlf.blockdaemon.io
-helm install peer --set PeerNum=1 --set OrgName=prod --set DomainName=hlf.blockdaemon.io
+helm install orderer --set DomainName=${DOMAIN}
+helm install peer --set PeerNum=0 --set OrgName=${SUBDOMAIN1} --set DomainName=${DOMAIN}
+helm install peer --set PeerNum=1 --set OrgName=${SUBDOMAIN1} --set DomainName=${DOMAIN}
