89e7cf5dc806d6b3c1d58adfd56404e3f4b4adfa
Nye Liu <nye@blockdaemon.com>
2018-12-18 13:31:28 -0800

    Don't hardcode domain in helm charts

--- helm/start.sh
+++ helm/start.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-helm install orderer
-helm install peer --set PeerName=peer0 --set OrgName=prod
-helm install peer --set PeerName=peer1 --set OrgName=prod
+helm install orderer --set DomainName=hlf.blockdaemon.io
+helm install peer --set PeerName=peer0 --set OrgName=prod --set DomainName=hlf.blockdaemon.io
+helm install peer --set PeerName=peer1 --set OrgName=prod --set DomainName=hlf.blockdaemon.io
