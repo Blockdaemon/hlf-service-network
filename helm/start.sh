d85105d186d32ab1538cc9e21d9abbdebb2967d2
Nye Liu <nye@blockdaemon.com>
2019-01-14 17:47:33 +0000

    More operations fixes
    * Orderer env is ORDERER_OPERATIONS_LISTENADDRESS not CORE_OPERATIONS_LISTENADDRESS
    * Don't hardcode fabric version in helm charts
    * Add liveness probes
    * Add prometheus annotations

--- helm/start.sh
+++ helm/start.sh
@@ -6,6 +6,6 @@ read-env
 
 set -xe
 
-helm install orderer --set DomainName=${DOMAIN}
-helm install peer --set PeerNum=0 --set OrgName=${SUBDOMAIN1} --set DomainName=${DOMAIN}
-helm install peer --set PeerNum=1 --set OrgName=${SUBDOMAIN1} --set DomainName=${DOMAIN}
+helm install orderer --set HLFTag="${HLF_ARCH}-${HLF_VERSION}" --set DomainName=${DOMAIN}
+helm install peer --set HLFTag="${HLF_ARCH}-${HLF_VERSION}" --set PeerNum=0 --set OrgName=${SUBDOMAIN1} --set DomainName=${DOMAIN}
+helm install peer ---set HLFTag="${HLF_ARCH}-${HLF_VERSION}" -set PeerNum=1 --set OrgName=${SUBDOMAIN1} --set DomainName=${DOMAIN}
