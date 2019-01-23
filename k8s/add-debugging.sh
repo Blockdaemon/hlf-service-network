d27a41c025ed582f49bae5fa2667727a597606f0
Nye Liu <nye@blockdaemon.com>
2019-01-23 01:47:19 +0000

    Remove hostnames, we don't need it

--- k8s/add-debugging.sh
+++ k8s/add-debugging.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-kubectl run hostnames --image=k8s.gcr.io/serve_hostname \
-                        --labels=app=hostnames \
-                        --labels=role=debug \
-                        --port=9376 \
-                        --replicas=3
+#kubectl run hostnames --image=k8s.gcr.io/serve_hostname \
+#                        --labels=app=hostnames \
+#                        --labels=role=debug \
+#                        --port=9376 \
+#                        --replicas=3
+#kubectl expose deployment hostnames --type NodePort --port=80 --target-port=9376 --labels=role=debug
 
+#kubectl delete deployment hostnames
 #kubectl delete service hostnames
-kubectl expose deployment hostnames --type NodePort --port=80 --target-port=9376 --labels=role=debug
 
 kubectl apply -f debug.yaml
 
